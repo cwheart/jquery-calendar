cbe713703bedba21cf359fbfffca1820f2837af7
cwheart <xinshuaifeng@126.com>
2016-09-08 16:12:37 +0800

    switch date

--- src/coffee/jquery-calendar.coffee
+++ src/coffee/jquery-calendar.coffee
@@ -7,6 +7,11 @@
     this.dayList = $(this).find(".calendar-list")
     this.nav = $(this).find(".calendar-nav")
     this.title = $(this).find(".calendar-title")
+    this.days = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"]
+    this.getDayStr = (day)->
+      day = 6 if day == 0
+      this.days[day - 1]
+
     obj = this
     this.nextMonth = ->
       this.initDate.setMonth(this.initDate.getMonth() + 1)
@@ -23,7 +28,7 @@
     # 星期一 星期二 星期三...
     this.drawTitle = ->
       $(this.title).empty()
-      for day in ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"]
+      for day in this.days
         $(this.title).append("<div>" + day + "</div>")
     this.drawList = ->
       tmp = new Date(this.initDate)
@@ -34,13 +39,24 @@
       $(this.dayList).empty()
       while 1
         tmp.setDate(tmp.getDate() + 1)
-        $(this.dayList).append("<div class=\"cal-cell\">"+ tmp.getDate +"</div>")
-        console.log(tmp.toDateString())
+        day = $("<div class=\"cal-cell\"></div>")
+        day.addClass("current") if tmp.toDateString() == this.initDate.toDateString()
+        day.append("<a data-date=\"" + tmp.toDateString() + "\" class=\"day-cell\" href=\"javascript:void(0);\">"+ tmp.getDate +"</a>")
+        $(this.dayList).append(day)
         break if (tmp.getFullYear() > this.initDate.getFullYear() || tmp.getMonth() > this.initDate.getMonth()) && tmp.getDay() == 0
     # 绘制导航
     this.drawNav = ->
       $(this.nav).empty()
-      $(this.nav).append("<h1><span class=\"prevMonth\"><a src=\"javascript:void(0);\">&lt;</a></span><span class=\"nextMonth\"><a src=\"javascript:void(0);\">&gt;</a></span></h1>")
+      selectedMonth = $("<h2></h2>")
+      $(this.nav).append(selectedMonth)
+      selectedMonth.append("<span class=\"prevMonth\"><a href=\"javascript:void(0);\">&lt;</a></span>")
+      selectedMonth.append("<label>" + this.initDate.getFullYear() + "年 " + (this.initDate.getMonth() + 1) + "月</label>")
+      selectedMonth.append("<span class=\"nextMonth\"><a href=\"javascript:void(0);\">&gt;</a></span>")
+      selectedDate = $("<div class=\"selected-date\"></div>")
+      $(this.nav).append(selectedDate)
+      selectedDate.text(this.initDate.getDate())
+      selectedDay = $("<div class=\"selected-day\">" + this.getDayStr(this.initDate.getDay()) + "</div>")
+      $(this.nav).append(selectedDay)
       
     # 选择日期
     # date like '2015-11-11'
@@ -56,5 +72,7 @@
       obj.nextMonth()
     $(this).on "click", ".prevMonth", ->
       obj.prevMonth()
+    $(this).on "click", ".day-cell", ->
+      obj.selectDate($(this).attr("data-date"))
     obj
 )(jQuery)
--- src/js/jquery-calendar.js
+++ src/js/jquery-calendar.js
@@ -7,6 +7,13 @@
     this.dayList = $(this).find(".calendar-list");
     this.nav = $(this).find(".calendar-nav");
     this.title = $(this).find(".calendar-title");
+    this.days = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"];
+    this.getDayStr = function(day) {
+      if (day === 0) {
+        day = 6;
+      }
+      return this.days[day - 1];
+    };
     obj = this;
     this.nextMonth = function() {
       this.initDate.setMonth(this.initDate.getMonth() + 1);
@@ -24,7 +31,7 @@
     this.drawTitle = function() {
       var day, i, len, ref, results;
       $(this.title).empty();
-      ref = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"];
+      ref = this.days;
       results = [];
       for (i = 0, len = ref.length; i < len; i++) {
         day = ref[i];
@@ -45,8 +52,15 @@
       results = [];
       while (1) {
         tmp.setDate(tmp.getDate() + 1);
-        $(this.dayList).append("<div class=\"cal-cell\">" + tmp.getDate(+"</div>"));
-        console.log(tmp.toDateString());
+        day = $("<div class=\"cal-cell\"></div>");
+        if (tmp.toDateString() === this.initDate.toDateString()) {
+          day.addClass("current");
+        }
+        if (tmp.toDateString() === this.initDate.toDateString()) {
+          console.log("currrrrrrrent");
+        }
+        day.append("<a data-date=\"" + tmp.toDateString() + "\" class=\"day-cell\" href=\"javascript:void(0);\">" + tmp.getDate(+"</a>"));
+        $(this.dayList).append(day);
         if ((tmp.getFullYear() > this.initDate.getFullYear() || tmp.getMonth() > this.initDate.getMonth()) && tmp.getDay() === 0) {
           break;
         } else {
@@ -56,8 +70,18 @@
       return results;
     };
     this.drawNav = function() {
+      var selectedDate, selectedDay, selectedMonth;
       $(this.nav).empty();
-      return $(this.nav).append("<h1><span class=\"prevMonth\"><a src=\"javascript:void(0);\">&lt;</a></span><span class=\"nextMonth\"><a src=\"javascript:void(0);\">&gt;</a></span></h1>");
+      selectedMonth = $("<h2></h2>");
+      $(this.nav).append(selectedMonth);
+      selectedMonth.append("<span class=\"prevMonth\"><a href=\"javascript:void(0);\">&lt;</a></span>");
+      selectedMonth.append("<label>" + this.initDate.getFullYear() + "年 " + (this.initDate.getMonth() + 1) + "月</label>");
+      selectedMonth.append("<span class=\"nextMonth\"><a href=\"javascript:void(0);\">&gt;</a></span>");
+      selectedDate = $("<div class=\"selected-date\"></div>");
+      $(this.nav).append(selectedDate);
+      selectedDate.text(this.initDate.getDate());
+      selectedDay = $("<div class=\"selected-day\">" + this.getDayStr(this.initDate.getDay()) + "</div>");
+      return $(this.nav).append(selectedDay);
     };
     this.selectDate = function(selectedDate) {
       var date;
@@ -73,6 +97,9 @@
     });
     $(this).on("click", ".prevMonth", function() {
       return obj.prevMonth();
+    });
+    $(this).on("click", ".day-cell", function() {
+      return obj.selectDate($(this).attr("data-date"));
     });
     return obj;
   };
